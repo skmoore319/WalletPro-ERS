f84a8241132d935f01a7756f3b048f79e3be38b1
Scott <sm.scottkmoore@gmail.com>
2018-06-09 11:40:17 -0400

    Add a sign-in component. TO DO: Make component functional with container, reducer, and actions

--- walletpro-ers-front-end/src/App.tsx
+++ walletpro-ers-front-end/src/App.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './Store';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { RequestListContainer } from './components/request-list/request-list.container';
+// import { SignInComponent } from './components/sign-in/sign-in.component';
 
 // import logo from './logo.svg';
 
@@ -17,6 +18,7 @@ class App extends React.Component {
           <div>
               <NavComponent />
               <Switch>
+                {/* <Route path="/log-in" component={SignInComponent} /> */}
                 <Route component={RequestListContainer} />
               </Switch>
           </div>
