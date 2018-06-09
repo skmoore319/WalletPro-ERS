47df4853d892d1e59bcb5ada63a5a4cd22dbc97c
Scott <sm.scottkmoore@gmail.com>
2018-06-08 21:31:21 -0400

    To do: Add login capabilities to the routers on the back end

--- walletpro-ers-front-end/src/components/request-list/request-list.component.tsx
+++ walletpro-ers-front-end/src/components/request-list/request-list.component.tsx
@@ -50,15 +50,41 @@ export class RequestListComponent extends React.Component<IProp, any> {
                                 <tbody>
                                     {
                                         this.props.requests.map(request => 
-                                            <tr key={request.username}>
-                                            <th>{request.timeSubmitted}</th>
-                                            <td>{request.items.size}</td>
-                                            <td>{request.status}</td>
-                                            <td>{request.approver}</td>
-                                            </tr>
+                                            
+                                                <tr key={request.username}>
+                                                <td>{request.username}</td>
+                                                <td>{request.timeSubmitted}</td>
+                                                <td>{request.items.length}</td>
+                                                <td>{request.status}</td>
+                                                <td>{request.approver}</td>
+                                                </tr>,
+                                                // <tr>
+                                                //     <td>Time Submitted</td>
+                                                //     <td>Title</td>
+                                                //     <td>Amount</td>
+                                                //     <td>Type</td>
+                                                //     <td>Description</td>
+                                                // </tr>,
+
+
+                                                // 6/8/2018
+                                                // Figure this out -- need to map yet another object, the item, within
+                                                // each reimbursement request line item -- I want to click on a request
+                                                // and see all the items on it
+                                                // {
+                                                //     request.items.map(item =>
+                                                //         <tr key={item.timeSubmitted}>
+                                                //             <td>{item.timeSubmitted}</td>
+                                                //             <td>{item.title}</td>
+                                                //             <td>{item.amount}</td>
+                                                //             <td>{item.type}</td>
+                                                //             <td>{item.description}</td>
+                                                //         </tr>
+                                                //     )
+                                                // }
                                             )
                                     }
-                                    <tr data-toggle="collapse" data-target="#more-info-1" className="accordion-toggle" >
+                                    {/* <tr data-toggle="collapse" data-target="#more-info-1" className="accordion-toggle" >
                                         <td>thing</td>
                                         <td>thing</td>
                                         <td>thing</td>
@@ -252,7 +278,7 @@ export class RequestListComponent extends React.Component<IProp, any> {
                                         <td>thing</td>
                                         <td>thing</td>
                                         <td>thing</td>
-                                    </tr>
+                                    </tr> */}
                                 </tbody>
                             </table>
                         </div>
